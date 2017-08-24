049c6be7c9f61e7da2af33319442e2bf6a2789df
Jeancarlos De la Cruz Criollo <jdelacruz@sapia.com.pe>
2017-08-23 23:21:35 -0500

    Se agrega theme Unify al sistema Se implementa login interactivo con vuejs Se implementa rol por usuario

---
+++ resources/views/errors/permission.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger fade in text-center">
+    <h4 class="text-bold"><i class="fa fa-warning text-warning"></i> No tienes acceso al modulo</h4>
+    <span>Favor de contactarse con un administrador</span>
+</div>
---
+++ resources/views/layouts/login/footer.blade.php
@@ -0,0 +1,16 @@
+<script src="{!! asset('js/app.js?version='.date('YmdHis'))!!}"></script>
+<script src="{!! asset('js/implement.js?version='.date('YmdHis'))!!}"></script>
+<script src="{!! asset('js/centinel.js?version='.date('YmdHis'))!!}"></script>
+<script>
+    $(function() {
+        Unify.init()
+        $.backstretch([
+            "assets/img/bg/19.jpg",
+            "assets/img/bg/18.jpg",
+        ], {
+            fade: 1000,
+            duration: 7000
+        })
+    })
+    formEnter('login-form',true)
+</script>
